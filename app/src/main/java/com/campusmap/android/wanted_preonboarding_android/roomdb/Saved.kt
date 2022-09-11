a8ab8a75f99ed289fa4d04eec71315179d0e47a0
choieuihyun <amammmaa1@naver.com>
2022-09-11 15:55:30 +0900

    2022-09-11 wanted_preonboarding_android
    MVVM_Refactoring second task
    
    TopNewsDetail, CategoryItemDetail MVVM Architecture completed
    
    Saved기능 구현 후, 즐겨찾기 기능 + 앱 껐다 켜도 유지 완료

---
+++ app/src/main/java/com/campusmap/android/wanted_preonboarding_android/roomdb/Saved.kt
@@ -0,0 +1,20 @@
+package com.campusmap.android.wanted_preonboarding_android.roomdb
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.campusmap.android.wanted_preonboarding_android.news.Article
+
+@Entity
+data class Saved (
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null, // 요렇게하고 null로 주면 autoGenerate
+    var author: String?,
+    var content: String?,
+    var publishedAt: String?,
+    var title: String?,
+    var urlToImage: String?,
+    var savedButton: Boolean = false
+) {
+
+}
