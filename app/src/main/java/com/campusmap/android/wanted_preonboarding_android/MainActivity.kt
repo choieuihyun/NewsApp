47c328232569e998f779ab802dbb90e671e44718
choieuihyun <amammmaa1@naver.com>
2022-09-20 13:41:07 +0900

    practice branch에 practice_2 branch 생성

--- app/src/main/java/com/campusmap/android/wanted_preonboarding_android/MainActivity.kt
+++ app/src/main/java/com/campusmap/android/wanted_preonboarding_android/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 여기 practice2 추가요
 
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
 
