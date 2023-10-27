f4b1d5956ee45c9fc2dd41a2f2d5662fe8bb04e0
choeuihyeon <amammmaa1@naver.com>
2023-10-27 23:07:22 +0900

    feature : 각 presenter를 포함하고 있는 MainContract Interface 구현, Google 공식에 따름.

---
+++ presentation/src/main/java/com/example/presentation/adapter/BindingAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.presentation.adapter
+
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+//object BindingAdapter{
+//    @JvmStatic
+//    @BindingAdapter("imageUrl")
+//    fun bindImage(imageView: ImageView, url: String?) {
+//        Glide.with(imageView.context).load(url).into(imageView)
+//    }
+//}
+
+
+
+
