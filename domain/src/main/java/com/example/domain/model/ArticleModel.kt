fcf6e8193973ee8b4d5ef79f8c5e3d5902b890d1
choeuihyeon <amammmaa1@naver.com>
2023-10-28 23:46:08 +0900

    feature : Category -> CategoryDetail 간의 데이터 전송을 위한 직렬화 데이터 클래스 생성

--- domain/src/main/java/com/example/domain/model/ArticleModel.kt
+++ domain/src/main/java/com/example/domain/model/ArticleModel.kt
@@ -1,5 +1,8 @@
 package com.example.domain.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class TopNewsResponseModel(
 
     val articles: List<Any?>?, // 매퍼에서 에러떠서 List<ArticleModel?>?로 안했음.
@@ -28,6 +31,73 @@ data class ArticleModel (
     val urlToImage: String?,
 
     )
+
+data class ArticleModelParcelize ( // topNews -> topNewsDetail 데이터 전달하려고 만듦
+                                    // 직렬화 시킨 데이터는 내부에 접근할 수가 없어서..
+    val author: String?,
+
+    val content: String?,
+
+    val publishedAt: String?,
+
+    val title: String?,
+
+    val urlToImage: String?,
+
+    ) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        author = parcel.readString(),
+        content = parcel.readString(),
+        publishedAt = parcel.readString(),
+        title = parcel.readString(),
+        urlToImage = parcel.readString()
+    )
+
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeString(author)
+        parcel.writeString(publishedAt)
+        parcel.writeString(urlToImage)
+        parcel.writeString(content)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ArticleModelParcelize> {
+        override fun createFromParcel(parcel: Parcel): ArticleModelParcelize {
+            return ArticleModelParcelize(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ArticleModelParcelize?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+    }
+
+//        fun getTitle(): String? {
+//            return title
+//        }
+//
+//        fun getAuthor(): String? {
+//            return author
+//        }
+//
+//        fun getContent(): String? {
+//            return content
+//        }
+//
+//        fun getPublishedAt(): String? {
+//            return publishedAt
+//        }
+//
+//        fun getImageUrl(): String? {
+//            return urlToImage
+//        }
 
 data class SourceModel(
 
