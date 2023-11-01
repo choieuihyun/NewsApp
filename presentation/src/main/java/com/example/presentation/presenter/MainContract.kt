9c0eeb018b3df2f3a9f0a92e1c0a704444448773
choeuihyeon <amammmaa1@naver.com>
2023-11-01 21:38:47 +0900

    feature : 역할 별 인터페이스 세분화

--- presentation/src/main/java/com/example/presentation/presenter/MainContract.kt
+++ presentation/src/main/java/com/example/presentation/presenter/MainContract.kt
@@ -7,31 +7,64 @@ import com.example.domain.model.SavedModel
 interface MainContract {
 
     interface TopNewsView<T> { // view에서 ui update를 하기위한 method 선언
+
         fun updateNews(topNews: List<T?>?)
-        //fun updateNews(topNews: List<T?>?)
+
     }
 
     interface TopNewsPresenter { // business logic 선언
         
         suspend fun getTopNews()
 
-        interface TopNewsDetailPresenter {
+
+    }
+
+    interface TopNewsDetailAddPresenter { // TopNewsPresenter 내부에서 분리.
 
             suspend fun addTopNews(saved: SavedModel)
 
-        }
     }
+
+    interface TopNewsDetailDeletePresenter {
+
+        suspend fun deleteTopNews(title: String)
+
+    }
+
 
     interface TopNewsCategoryPresenter {
         suspend fun getTopNewsCategory(category: String)
-        suspend fun savedTopNewsCategory()
+
     }
 
+    interface TopNewsCategoryDetailAddPresenter {
+
+        suspend fun addTopNewsCategory(saved: SavedModel)
+    }
+
+    interface TopNewsCategoryDetailDeletePresenter {
+
+        suspend fun deleteTopNewsCategory(title: String)
+
+    }
+
+
+    // 즐겨찾기
     interface TopNewsSavedPresenter {
+
         suspend fun getTopNewsSaved()
+
+    }
+
+    interface TopNewsSavedDetailAddPresenter {
         suspend fun addTopNewsSaved(saved: SavedModel)
 
+    }
+
+    interface TopNewsSavedDetailDeletePresenter {
+
         suspend fun deleteTopNewsSaved(title: String)
+
     }
 
 }
