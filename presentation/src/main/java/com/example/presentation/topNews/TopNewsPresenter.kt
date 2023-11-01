22f1774cef90fbc0abfa973457eabb52b0d8ac1b
choeuihyeon <amammmaa1@naver.com>
2023-11-01 21:53:55 +0900

    feature : 뉴스 목록(TopNews) view presenter 구현

--- presentation/src/main/java/com/example/presentation/topNews/TopNewsPresenter.kt
+++ presentation/src/main/java/com/example/presentation/topNews/TopNewsPresenter.kt
@@ -1,9 +1,9 @@
-package com.example.presentation.presenter
+package com.example.presentation.topNews
 
 import android.util.Log
 import com.example.domain.model.ArticleModel
-import com.example.domain.model.SavedModel
 import com.example.domain.usecase.GetTopNewsUseCase
+import com.example.presentation.presenter.MainContract
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
